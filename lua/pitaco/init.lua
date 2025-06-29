e06ff3f1db95258797724c5828b01a3c4b271f67
Joel Xavier Rocha <joelxr@gmail.com>
2025-06-29 20:46:15 -0300

    feat: Add OpenRouter provider support to pitaco plugin

--- lua/pitaco/init.lua
+++ lua/pitaco/init.lua
@@ -3,6 +3,7 @@ local M = {}
 local default_opts = {
   anthropic_model_id = "claude-3-5-haiku-latest",
 	openai_model_id = "gpt-4.1-mini",
+  openrouter_model_id = "openrouter/deepseek/deepseek-chat-v3-0324:free",
   provider = "anthropic",
 	language = "english",
 	additional_instruction = nil,
@@ -13,6 +14,7 @@ function M.setup(opts)
 	opts = vim.tbl_deep_extend("force", default_opts, opts or {})
   vim.g.pitaco_provider = opts.provider
   vim.g.pitaco_anthropic_model_id = opts.anthropic_model_id
+  vim.g.pitaco_openrouter_model_id = opts.openrouter_model_id
 	vim.g.pitaco_openai_model_id = opts.openai_model_id
 	vim.g.pitaco_language = opts.language
 	vim.g.pitaco_additional_instruction = opts.additional_instruction
